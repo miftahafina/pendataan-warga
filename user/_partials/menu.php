960aac5c3d6f9bba4e7d0b05bc6627cc8c67526d
Miftah Afina <miftah.afina@gmail.com>
2016-06-14 05:36:09 +0700

    Penambahan panel pada menu

--- user/_partials/menu.php
+++ user/_partials/menu.php
@@ -1,18 +1,22 @@
-<div class="row">
-  <div class="col-md-2">
-    <a href="create.php" class="btn btn-primary">
-      <i class="glyphicon glyphicon-file"></i> Tambah
-    </a>
-  </div>
-  <div class="col-md-2">
-    <a href="index.php" class="btn btn-primary">
-      <i class="glyphicon glyphicon-eye-open"></i> Lihat Data
-    </a>
-  </div>
-  <div class="col-md-2">
-    <a href="#" class="btn btn-primary">
-      <i class="glyphicon glyphicon-refresh"></i> Refresh
-    </a>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-md-2">
+        <a href="create.php" class="btn btn-primary">
+          <i class="glyphicon glyphicon-file"></i> Tambah
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="index.php" class="btn btn-primary">
+          <i class="glyphicon glyphicon-eye-open"></i> Lihat Data
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="#" class="btn btn-primary">
+          <i class="glyphicon glyphicon-refresh"></i> Refresh
+        </a>
+      </div>
+    </div>
   </div>
 </div>
 <br>
