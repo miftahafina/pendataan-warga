f075e12c8a9f61fbc628754f0225de33dea15670
Miftah Afina <miftah.afina@gmail.com>
2016-06-15 23:33:51 +0700

    PHP upload file masih gagal

--- pages/galeri/create.php
+++ pages/galeri/create.php
@@ -3,42 +3,23 @@
 <h1 class="page-header">Galeri</h1>
 <?php include('_partials/menu.php') ?>
 
-<form action="" method="post">
+<form action="store.php" method="post" enctype="multipart/form-data">
 <h3>A. Data Pribadi</h3>
 <table class="table table-striped table-middle">
   <tr>
     <th width="20%">Path</th>
     <td width="1%">:</td>
-    <td><input type="file" class="form-control" name="_galeri"></td>
+    <td><input type="file" class="form-control" name="path_galeri" required></td>
   </tr>
   <tr>
     <th>Caption</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="_galeri"></td>
+    <td><input type="text" class="form-control" name="caption_galeri" required></td>
   </tr>
   <tr>
     <th>Tautan</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="_galeri"></td>
-  </tr>
-</table>
-
-<h3>B. Data Aplikasi</h3>
-<table class="table table-striped table-middle">
-  <tr>
-    <th width="20%">Diinput oleh</th>
-    <td width="1%">:</td>
-    <td><input type="text" class="form-control" name="_galeri"></td>
-  </tr>
-  <tr>
-    <th>Diinput</th>
-    <td>:</td>
-    <td><input type="text" class="form-control" name="_galeri"></td>
-  </tr>
-  <tr>
-    <th>Diperbaharui</th>
-    <td>:</td>
-    <td><input type="text" class="form-control" name="_galeri"></td>
+    <td><input type="text" class="form-control" name="tautan_galeri"></td>
   </tr>
 </table>
 
