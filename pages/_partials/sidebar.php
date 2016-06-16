5065800927b39dddfbc77e9fd06318eb37f26c80
Miftah Afina <miftah.afina@gmail.com>
2016-06-16 11:05:55 +0700

    Active class sidebar

--- pages/_partials/sidebar.php
+++ pages/_partials/sidebar.php
@@ -1,16 +1,38 @@
+<?php
+function is_active($page) {
+  $uri = "$_SERVER[REQUEST_URI]";
+  if (strpos($uri, $page)) {
+    echo 'active';
+  }
+}
+?>
+
 <ul class="nav nav-sidebar">
-  <li class="active">
+  <li class="<?php is_active('dasbor'); ?>">
     <a href="../dasbor"><i class="glyphicon glyphicon-home"></i> Dasbor</a>
   </li>
 </ul>
+
 <ul class="nav nav-sidebar">
-  <li><a href="../warga"><i class="glyphicon glyphicon-book"></i> Data Warga</a></li>
-  <li><a href="../kartu-keluarga"><i class="glyphicon glyphicon-inbox"></i> Data Kartu Keluarga</a></li>
-  <li><a href="../mutasi"><i class="glyphicon glyphicon-export"></i> Data Mutasi</a></li>
+  <li class="<?php is_active('warga'); ?>">
+    <a href="../warga"><i class="glyphicon glyphicon-book"></i> Data Warga</a>
+  </li>
+  <li class="<?php is_active('kartu-keluarga'); ?>">
+    <a href="../kartu-keluarga"><i class="glyphicon glyphicon-inbox"></i> Data Kartu Keluarga</a>
+  </li>
+  <li class="<?php is_active('mutasi'); ?>">
+    <a href="../mutasi"><i class="glyphicon glyphicon-export"></i> Data Mutasi</a>
+  </li>
 </ul>
+
 <ul class="nav nav-sidebar">
-  <li><a href="../galeri"><i class="glyphicon glyphicon-picture"></i> Galeri</a></li>
+  <li class="<?php is_active('galeri'); ?>">
+    <a href="../galeri"><i class="glyphicon glyphicon-picture"></i> Galeri</a>
+  </li>
 </ul>
+
 <ul class="nav nav-sidebar">
-  <li><a href="../user"><i class="glyphicon glyphicon-user"></i> User</a></li>
+  <li class="<?php is_active('user'); ?>">
+    <a href="../user"><i class="glyphicon glyphicon-user"></i> User</a>
+  </li>
 </ul>
