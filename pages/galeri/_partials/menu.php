a3cfdf51011c13bf20780e2d88a073ef090c7442
Miftah Afina <miftah.afina@gmail.com>
2016-06-17 14:50:43 +0700

    Lightbox!

--- pages/galeri/_partials/menu.php
+++ pages/galeri/_partials/menu.php
@@ -11,11 +11,6 @@
           <i class="glyphicon glyphicon-eye-open"></i> Lihat Data
         </a>
       </div>
-      <div class="col-md-2">
-        <a href="#" class="btn btn-primary">
-          <i class="glyphicon glyphicon-refresh"></i> Refresh
-        </a>
-      </div>
     </div>
   </div>
 </div>
